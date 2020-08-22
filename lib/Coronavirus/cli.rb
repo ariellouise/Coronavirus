05b0e4b0375861da14efa99df6fdcf36c0bc7ab9
ariel murphy <arielladders@gmail.com>
2020-08-22 03:55:31 +0000

    modified cli

--- lib/Coronavirus/cli.rb
+++ lib/Coronavirus/cli.rb
@@ -12,8 +12,21 @@ class CLI
     puts "1. Total Cases"
     puts "2. Deceased Cases"
     puts "3. Recovered Cases"
+    puts "4. Exit Program"
+    choice = gets.strip
+    
+    case "1"
+     total_cases 
+    when "2"
+     deaths
+    when "3"
+     recovered 
+    when "4"
+     end_program 
+    end
   end 
-  #needs case statement regarding case "1" puts ...
+    
+
   def error 
     puts "Sorry, that answer was invalid. Please try again!"
   end 
